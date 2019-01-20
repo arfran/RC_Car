71c46667d054522fc06808c6b568f4ad2073ea64
agula97 <41758272+agula97@users.noreply.github.com>
2019-01-20 00:02:18 -0500

    Added MotorHandler class

---
+++ MotorDriver/MotorDriver_v4_4_a/MotorHandler.cpp
@@ -0,0 +1,55 @@
+class MotorHandler{
+  public:
+
+  void motorForward();
+  void motorReverse();
+  void motorLeft();
+  void motorRight();
+
+
+  
+};
+
+
+void MotorHandler::motorForward(){
+   digitalWrite(22,HIGH); //1     11,12 control right motor
+   digitalWrite(21,LOW);  //0
+   analogWrite(15,duty); //right motor
+   digitalWrite(20,HIGH);  //1    8,9 control left motor
+   digitalWrite(19,LOW);   //0
+   analogWrite(14,duty); //left motor
+   
+   
+}
+
+void MotorHandler::motorReverse(){
+   digitalWrite(22,LOW);  //0
+   digitalWrite(21,HIGH); //1
+   analogWrite(15,duty); //right
+   digitalWrite(20,LOW);   //0
+   digitalWrite(19,HIGH);  //1
+   analogWrite(14,duty); //left 
+}
+
+void MotorHandler::motorLeft(){
+   digitalWrite(22,HIGH);  //1
+   digitalWrite(21,LOW);   //0
+   analogWrite(15,duty); //right
+   digitalWrite(20,LOW);    //1, make left wheels turn reverse for larger pivot
+   digitalWrite(19,HIGH);    //0
+   analogWrite(14,(int)(duty/2)); //left; cut duty cycle by half to allow the car to effectively turn better
+
+  
+}
+
+void MotorHandler::motorRight(){
+
+  digitalWrite(22,LOW); //1, make right wheel turn reverse for larger pivot
+  digitalWrite(21,HIGH); //0
+  analogWrite(15,(duty/2)); //right; cut duty cycle by half to allow the car to effectively turn better
+  digitalWrite(20,HIGH); //1
+  digitalWrite(19,LOW);  //0
+  analogWrite(14,duty); //left
+
+  
+}
